0e8ed9628c29c29af5663b89504f34e23e277668
Tommy Zarick <zarick.6@osu.edu>
2022-01-07 22:09:02 -0500

    calculate debts + test

--- evenup/evenup.go
+++ evenup/evenup.go
@@ -1,11 +1,60 @@
 package evenup
 
+import (
+	"fmt"
+)
+
 type InitialLedger struct {
 	Names         []string
 	PaymentValues []float32
 }
 
 // for a given inital ledger (names and initial payments), return a list of transactions that evens everyone up
-func Calculate(initialLedger InitialLedger) (txList []string) {
+func CalculateTransactions(initialLedger InitialLedger) ([]string, error) {
+	debts := calculateDebts(initialLedger.PaymentValues)
+	err := validateDebts(debts)
+	if err != nil {
+		return []string{}, err
+	}
+
+	// buildTransactionsMap - maybe name this better -> buildTransactionList
+	return []string{}, nil
+}
+
+// for all initial payment values, calculate the amount the person owes (positive value) or is owed (negative value)
+func calculateDebts(payments []float32) []float32 {
+	debts := make([]float32, len(payments))
+	evenAmount := sum(payments) / float32(len(payments))
+
+	for i, payment := range payments {
+		debts[i] = evenAmount - payment
+	}
+
+	return debts
+}
+
+func buildTransactionList(debts []float32, friends []string) []string {
 	return []string{}
+}
+
+func validateDebts(debts []float32) error {
+	if !isEffectivelyZero(sum(debts)) {
+		return fmt.Errorf("debts must add up to zero")
+	}
+
+	return nil
+}
+
+// the granularity we care about is a max differential of 0.01 between values. float arithmetic may cause problems if we don't specify this
+func isEffectivelyZero(num float32) bool {
+	return num > -0.01 && num < 0.01
+}
+
+func sum(vals []float32) float32 {
+	var sum float32
+	for _, v := range vals {
+		sum += v
+	}
+
+	return sum
 }
--- evenup/evenup_test.go
+++ evenup/evenup_test.go
@@ -1,1 +1,37 @@
-package evenup_test
+package evenup
+
+import (
+	"testing"
+)
+
+type DebtTestCase struct {
+	id            int
+	inputPayments []float32
+	expected      []float32
+}
+
+func TestCalculateDebts(t *testing.T) {
+	cases := []DebtTestCase{
+		{
+			id:            1,
+			inputPayments: []float32{60, 75, 20, 0},
+			expected:      []float32{-21.25, -36.25, 18.75, 38.75},
+		},
+		{
+			id:            2,
+			inputPayments: []float32{100, 72.5, 20, 7, 0},
+			expected:      []float32{-60.1, -32.6, 19.9, 32.9, 39.9},
+		},
+	}
+
+	for _, test := range cases {
+		t.Logf("Running test case #%v...\n", test.id)
+		got := calculateDebts(test.inputPayments)
+		for i := range test.expected {
+			if !isEffectivelyZero(got[i] - test.expected[i]) {
+				t.Errorf("Test case #%v failed: Expected: %v, Got: %v", test.id, test.expected, got)
+				t.FailNow()
+			}
+		}
+	}
+}
