2246a35cc2bb11999bad32e0adc47a8e47712d72
Tommy Zarick <zarick.6@osu.edu>
2022-01-08 00:35:45 -0500

    all tests passing

--- evenup/evenup.go
+++ evenup/evenup.go
@@ -2,6 +2,8 @@ package evenup
 
 import (
 	"fmt"
+	"math"
+	"sort"
 )
 
 type InitialLedger struct {
@@ -17,8 +19,9 @@ func CalculateTransactions(initialLedger InitialLedger) ([]string, error) {
 		return []string{}, err
 	}
 
-	// buildTransactionsMap - maybe name this better -> buildTransactionList
-	return []string{}, nil
+	transactions := buildTransactionList(debts, initialLedger.Names)
+
+	return transactions, nil
 }
 
 // for all initial payment values, calculate the amount the person owes (positive value) or is owed (negative value)
@@ -34,19 +37,116 @@ func calculateDebts(payments []float32) []float32 {
 }
 
 func buildTransactionList(debts []float32, friends []string) []string {
-	return []string{}
+	sortFriendsByDebt(friends, debts)
+	var transactions []string
+
+	i := 0              // left side pointer
+	j := len(debts) - 1 // right side pointer
+	for i < j {
+		transactions = clearEasyMatches(debts, friends, transactions, i, j)
+
+		if isZero(debts[i]) && isZero(debts[j]) {
+			i++
+			j--
+			continue
+		}
+
+		largerAbsValIdx := -1
+		smallerAbsValIdx := -1
+
+		if abs(debts[i]) <= abs(debts[j]) {
+			smallerAbsValIdx = i
+			largerAbsValIdx = j
+		} else {
+			smallerAbsValIdx = j
+			largerAbsValIdx = i
+		}
+
+		txAmount := abs(debts[smallerAbsValIdx])
+
+		// adjust values based on transaction amount.
+		// make sure our signs are right by checking whether or not the left side is positive or negative
+		// (aka i or j -> implicit: val at index i should always be neg [is owed], val at index j should always be pos [owes])
+		alteration := float32(txAmount) // this is the amount that we alter the larger value by
+		if largerAbsValIdx == i {
+			alteration = alteration * -1
+		}
+
+		debts[largerAbsValIdx] -= alteration
+		debts[smallerAbsValIdx] = 0
+
+		transactions = append(transactions, fmt.Sprintf("%s pays %s $%.2f", friends[j], friends[i], txAmount))
+
+		// adjust left and right pointers
+		for isZero(debts[i]) && i < len(debts)-1 {
+			i++
+		}
+
+		for isZero(debts[j]) && j > 0 {
+			j--
+		}
+	}
+
+	return transactions
+}
+
+// sort both debts and friends based on debts in ascending order (neg to pos === [is owed] to [owes])
+func sortFriendsByDebt(friends []string, debts []float32) {
+	// group the 2 fields together so we can sort them together
+	friendDebts := make([]struct {
+		friend string
+		debt   float32
+	}, len(friends))
+
+	for i := range friends {
+		friendDebts[i].friend = friends[i]
+		friendDebts[i].debt = debts[i]
+	}
+
+	sort.Slice(friendDebts, func(i, j int) bool {
+		return friendDebts[i].debt < friendDebts[j].debt // asc
+	})
+
+	// alter the underlying friends and debts data structures
+	for i := range friendDebts {
+		friends[i] = friendDebts[i].friend
+		debts[i] = friendDebts[i].debt
+	}
+}
+
+// clear the easy matches (any owe <--> owed equalities). These are the quick low hanging fruit that the main alg may miss, without which could result in extra transactions.
+// modifies debts
+// assumes debts is sorted asc
+func clearEasyMatches(debts []float32, friends []string, transactions []string, i, j int) []string {
+	for debts[i] < 0 {
+		for debts[j] >= abs(debts[i]) {
+			if isZero(debts[j] - abs(debts[i])) {
+				transactions = append(transactions, fmt.Sprintf("%s pays %s $%.2f", friends[j], friends[i], debts[j]))
+
+				debts[i] = 0
+				debts[j] = 0
+
+				break
+			}
+			j--
+		}
+		i++
+	}
+
+	return transactions
 }
 
 func validateDebts(debts []float32) error {
-	if !isEffectivelyZero(sum(debts)) {
+	if !isZero(sum(debts)) {
 		return fmt.Errorf("debts must add up to zero")
 	}
 
 	return nil
 }
 
-// the granularity we care about is a max differential of 0.01 between values. float arithmetic may cause problems if we don't specify this
-func isEffectivelyZero(num float32) bool {
+// the granularity we care about is a max differential of 0.01 between values. float arithmetic may cause problems if we don't specify this.
+// this is *effectively* zero for our purposes
+func isZero(num float32) bool {
 	return num > -0.01 && num < 0.01
 }
 
@@ -57,4 +157,8 @@ func sum(vals []float32) float32 {
 	}
 
 	return sum
+}
+
+func abs(num float32) float32 {
+	return float32(math.Abs(float64(num)))
 }
--- evenup/evenup_test.go
+++ evenup/evenup_test.go
@@ -1,6 +1,7 @@
 package evenup
 
 import (
+	"reflect"
 	"testing"
 )
 
@@ -25,13 +26,67 @@ func TestCalculateDebts(t *testing.T) {
 	}
 
 	for _, test := range cases {
-		t.Logf("Running test case #%v...\n", test.id)
+		t.Logf("Running test case #%v\n", test.id)
 		got := calculateDebts(test.inputPayments)
 		for i := range test.expected {
-			if !isEffectivelyZero(got[i] - test.expected[i]) {
+			if !isZero(got[i] - test.expected[i]) {
 				t.Errorf("Test case #%v failed: Expected: %v, Got: %v", test.id, test.expected, got)
-				t.FailNow()
+				continue
 			}
+		}
+	}
+}
+
+type TxListTestCase struct {
+	id           int
+	inputDebts   []float32
+	inputFriends []string
+	expected     []string
+}
+
+func TestDebtValidator(t *testing.T) {
+	t.Logf("Running debt validator test #1")
+	case1 := []float32{30, 20.43, -100, 30, -23.77}
+	if err := validateDebts(case1); err == nil {
+		t.Errorf("Test case #1: Expected an error to be returned but got none for debt slice: %v", case1)
+	}
+
+	case2 := []float32{-21.25, -36.25, 18.75, 38.75}
+	if err := validateDebts(case2); err != nil {
+		t.Errorf("Test case #2: Did not expect error to be returned but got one for debt slice: %v", case2)
+	}
+}
+
+func TestBuildTransactionList(t *testing.T) {
+	cases := []TxListTestCase{
+		{
+			id:           1,
+			inputDebts:   []float32{-21.25, -36.25, 18.75, 38.75},
+			inputFriends: []string{"eve", "mitch", "bethie", "randy"},
+			expected: []string{
+				"randy pays mitch $36.25",
+				"randy pays eve $2.50",
+				"bethie pays eve $18.75",
+			},
+		},
+		// this case demonstrate how the clearEasyMatches logic is an important optimization (optimize = guarantee we find the solution with the smallest possible # of transactions. Actual performance will probably take a hit.)
+		{
+			id:           2,
+			inputDebts:   []float32{-50, -20, -10, 20, 60}, // if we were to pay down from the ends without considering the rest of the debts, we would miss the matching $20 debts (an easy win), leading to an extra transaction in the end.
+			inputFriends: []string{"mal", "tessa", "sam", "scarlett", "mary"},
+			expected: []string{
+				"scarlett pays tessa $20.00",
+				"mary pays mal $50.00",
+				"mary pays sam $10.00",
+			},
+		},
+	}
+
+	for _, test := range cases {
+		t.Logf("Running testcase #%v\n", test.id)
+		got := buildTransactionList(test.inputDebts, test.inputFriends)
+		if !reflect.DeepEqual(got, test.expected) {
+			t.Errorf("Test case #%v failed: Expected: %v, Got: %v", test.id, test.expected, got)
 		}
 	}
 }
